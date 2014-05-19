31f4e644f9fe3499f42c3597d8c8b7b632b65d74
Andy Nicholson <andrew@anicholson.net>
2014-05-19 13:38:21 +1000

    Add name to Location

--- lib/mining_depot/entities/location.rb
+++ lib/mining_depot/entities/location.rb
@@ -2,11 +2,17 @@ require 'mining_depot/entity'
 require 'hashie'
 
 class Location < MiningDepot::Entity
-  attr_reader :coordinates
+  attr_reader :coordinates, :name
 
   def initialize(params = {})
-    x = params[:x] || 0
-    y = params[:y] || 0
+    x    = params[:x] || 0
+    y    = params[:y] || 0
+    @name = params[:name]
     @coordinates = Hashie::Mash.new(x: x, y: y)
+  end
+
+  def name
+    name = @name || 'Location'
+    "#{name} (#{coordinates.x}, #{coordinates.y})"
   end
 end
--- spec/entities/location_spec.rb
+++ spec/entities/location_spec.rb
@@ -18,17 +18,37 @@ describe Location do
     it { should respond_to(:x)  }
     it { should respond_to(:y)  }
 
+    it 'default to the origin' do
+      expect(subject.x).to eq(0)
+      expect(subject.y).to eq(0)
+    end
+
     context 'provided' do
       let(:x) { 9 }
       let(:y) { 2 }
       its(:x) { should eq(9) }
       its(:y) { should eq(2) }
     end
+  end
 
-    context 'not provided' do
-      it 'defaults to the origin' do
-        expect(subject.x).to eq(0)
-        expect(subject.y).to eq(0)
+  it 'has a name' do
+    should respond_to :name
+  end
+
+  describe 'name' do
+    it 'defaults to "Location " + coordinates' do
+      expect(subject.name).to eq('Location (0, 0)')
+    end
+
+    context 'when provided' do
+      subject { Location.new name: 'Gemfields', x: 5, y:5 }
+
+      it 'contains the provided name' do
+        expect(subject.name).to match(/Gemfields/)
+      end
+
+      it 'contains the coordinates' do
+        expect(subject.name).to match /5, 5/
       end
     end
   end
