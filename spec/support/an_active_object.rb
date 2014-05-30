c94b58d97f1e8abd7b3fbc9c9473f6654b493756
Andy Nicholson <andrew@anicholson.net>
2014-05-30 10:58:26 +1000

    Make some protected methods available in testing

--- spec/support/an_active_object.rb
+++ spec/support/an_active_object.rb
@@ -4,6 +4,7 @@ shared_examples_for 'an active object' do
   let(:child_class) { Class.new(described_class) }
 
   subject { child_class.new }
+  before  { child_class.send(:public, *child_class.protected_instance_methods) }
   it { should respond_to :semaphore }
   its(:semaphore) { should be_a Mutex }
 end
