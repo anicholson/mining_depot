5598017ab303f55ec54de576ce386b82a375ba47
Andy Nicholson <andrew@anicholson.net>
2014-08-27 17:38:13 +1000

    Deleted unnecessary tests from StartMine spec

--- spec/interactors/start_mine_spec.rb
+++ spec/interactors/start_mine_spec.rb
@@ -4,29 +4,15 @@ describe StartMine do
   it_behaves_like 'an Interactor'
 
   describe 'required inputs:' do
-    let(:mine) { Mine.new minerals: :silver }
+    let(:mine) { Mine.new }
     subject { described_class.run(input)}
 
     describe 'a mine to start' do
-      context 'when provided' do
-        let(:input) {{ mine: mine }}
+      let(:input) {{ mine: mine }}
 
-        it 'runs' do
-          StartMine.any_instance.should_receive(:execute)
-
-          subject
-        end
-
-        it 'starts the mine' do
-          mine.should_receive :start
-
-          subject
-        end
-      end
-
-      context 'when omitted' do
-        let(:input) {{ mine: nil }}
-        it { should_not be_a_success }
+      it 'starts the mine' do
+        mine.should_receive :start
+        subject
       end
     end
   end
